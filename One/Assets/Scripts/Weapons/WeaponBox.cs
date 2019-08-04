68e705f39d5c3afcdf1ae6c758aacb82874fcc88
Unknown <kylengelmann@gmail.com>
2019-08-04 13:19:22 -0500

    weapon box timer

--- One/Assets/Scripts/Weapons/WeaponBox.cs
+++ One/Assets/Scripts/Weapons/WeaponBox.cs
@@ -8,13 +8,47 @@ public class WeaponBox : MonoBehaviour
     public int uses = -1;
     public PooledObjectType weaponType = PooledObjectType.Pistol;
 
+    public SpriteRenderer sprite;
+    public float respawnTime = 5f;
+    bool isActive = true;
+
+    IEnumerator OnUse()
+    {
+        isActive = false;
+        float time = 0;
+        sprite.enabled = false;
+        while (time < respawnTime)
+        {
+            time += TimeManager.GetTimeDelta(TimeChannel.Player);
+            yield return null;
+        }
+        sprite.enabled = true;
+        isActive = true;
+        if(curOther)
+        OnTriggerEnter(curOther);
+    }
+
+    Collider curOther;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other == curOther) curOther = null;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+
         PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+        if(!isActive)
+        {
+            curOther = other;
+            return;
+        }
         if(player)
         {
             if(player.PickupWeapon(weaponType))
             {
+                StartCoroutine(OnUse());
                 if(uses > 0)
                 {
                     --uses;
